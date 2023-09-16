7da1e207e66b0a1ae1bf87a55d3abc999919ccc3
its-relative <sapytomar10@gmail.com>
2023-09-16 08:35:03 +0530

    Added other DSA problems

---
+++ Untitles/Untitled-21.py
@@ -0,0 +1,4 @@
+sentence = input("Enter the sentence")
+words = sentence.split(" ")
+reverse = " ".join(words[::-1])
+print(reverse)
---
+++ Untitles/Untitled-22.py
@@ -0,0 +1,4 @@
+fruits = "apple,banana,cherry,date"
+# fruitl = ",".split(fruits)
+fruitl = fruits.split(",")
+print(fruitl)
